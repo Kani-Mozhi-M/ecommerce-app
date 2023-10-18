ead25287e76f018e318d6229fa26c3f58558996b
Kani-Mozhi-M <133675777+Kani-Mozhi-M@users.noreply.github.com>
2023-10-18 20:14:18 +0530

    ecommerce app

---
+++ src/products.js
@@ -0,0 +1,38 @@
+import product1 from "./assets/1.png";
+import product2 from "./assets/2.png";
+import product3 from "./assets/3.png";
+import product4 from "./assets/4.jpg";
+import product5 from "./assets/5.png";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "IPhone",
+    price: 999.0,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Macbook Pro 2022 (M1)",
+    price: 1999.0,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Cannon M50 Camera",
+    price: 699.0,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "Denim Jacket",
+    price: 228.0,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "tee",
+    price: 19.99,
+    productImage: product5,
+  },
+];
